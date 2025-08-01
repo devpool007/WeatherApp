be6ad0f8a118155b1ce2fddccf6abb8aed2b79f0
devpool007 <sharmadevansh007777@gmail.com>
2025-08-02 01:07:20 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Devansh Sharma on 02.08.25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
