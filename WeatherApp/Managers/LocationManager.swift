967314b9d220938d136437fd787840ff518f3761
devpool007 <sharmadevansh007777@gmail.com>
2025-08-02 09:56:45 +0200

    first commit

---
+++ WeatherApp/Managers/LocationManager.swift
@@ -0,0 +1,13 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by Devansh Sharma on 02.08.25.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+}
