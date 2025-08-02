2641a5aa8e91390891c7683189e5017679f419a5
devpool007 <sharmadevansh007777@gmail.com>
2025-08-02 22:18:50 +0200

    chore: fixed minor errors in managers

--- WeatherApp/Managers/WeatherManager.swift
+++ WeatherApp/Managers/WeatherManager.swift
@@ -9,13 +9,16 @@ import CoreLocation
 import Foundation
 
 class WeatherManager {
+    
+    let apiKey = Bundle.main.infoDictionary?["WEATHER_API_KEY"] as? String ?? ""
+
     func getCurrentWeather(
         latitude: CLLocationDegrees,
         longitude: CLLocationDegrees
     ) async throws -> ResponseBody {
         guard let url = URL(
                 string:
-                    "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=1cc71b1c053969ba722286c404f4b15c&units=metric"
+                    "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric"
             )
         else {
             fatalError("Missing URL")
@@ -33,37 +36,4 @@ class WeatherManager {
     }
 }
 
-// Model of the response body we get from calling the OpenWeather API
-//struct ResponseBody: Decodable {
-//    var coord: CoordinatesResponse
-//    var weather: [WeatherResponse]
-//    var main: MainResponse
-//    var name: String
-//    var wind: WindResponse
-//    
-//    struct CoordinatesResponse: Decodable {
-//        var lon: Double
-//        var lat: Double
-//    }
-//    
-//    struct WeatherResponse: Decodable {
-//        var id: Double
-//        var main: String
-//        var description: String
-//        var icon: String
-//    }
-//    
-//    struct MainResponse: Decodable {
-//        var temp: Double
-//        var feels_like: Double
-//        var temp_min: Double
-//        var temp_max: Double
-//        var pressure: Double
-//        var humidity: Double
-//    }
-//    
-//    struct WindResponse: Decodable {
-//        var speed: Double
-//        var deg: Double
-//    }
-//}
+
